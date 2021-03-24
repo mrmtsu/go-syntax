204c8508c4da77d39a8dfff0f181b3dd145b074a
mrmtsu <81202muramatsu@gmail.com>
2021-03-25 05:27:25 +0900

    byte型

--- main.go
+++ main.go
@@ -3,23 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var t, f bool = true, false
-	fmt.Println(t, f)
+	byteA := []byte{72, 73}
+	fmt.Println(byteA)
 
-	var s string = "Hello Golang"
-	fmt.Println(s)
-	fmt.Printf("%T\n", s)
+	fmt.Println(string(byteA))
 
-	var si string = "300"
-	fmt.Println(si)
-	fmt.Printf("%T\n", si)
+	c := []byte("HI")
+	fmt.Println(c)
 
-	fmt.Println(`test
-	test
-	    test`)
-
-	fmt.Println("\"")
-	fmt.Println(`"`)
-
-	fmt.Println(string(s[0]))
+	fmt.Println(string(c))
 }
