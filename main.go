f066d6b3c053eb4586d9075b4da77710d1b46e1f
mrmtsu <81202muramatsu@gmail.com>
2021-03-25 05:39:11 +0900

    インターフェイス型

--- main.go
+++ main.go
@@ -3,34 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var arr1 [3]int
-	fmt.Println(arr1)
-	fmt.Printf("%T\n", arr1)
+	var x interface{}
+	fmt.Println(x)
 
-	var arr2 [3]string = [3]string{"A", "B"}
-	fmt.Println(arr2)
+	x = 1
+	fmt.Println(x)
 
-	arr3 := [3]int{1, 2, 3}
-	fmt.Println(arr3)
+	x = 3.14
+	fmt.Println(x)
 
-	// 要素の数を自動でカウントする
-	arr4 := [...]string{"C", "D"}
-	fmt.Println(arr4)
-	fmt.Printf("%T\n", arr4)
+	x = "A"
+	fmt.Println(x)
 
-	fmt.Println(arr2[0])
-	fmt.Println(arr2[1])
-	fmt.Println(arr2[2])
-	fmt.Println(arr2[2-1])
+	x = [3]int{1, 2, 3}
+	fmt.Println(x)
 
-	arr2[2] = "C"
-	fmt.Println(arr2)
-
-	// 要素数が異なると別の型と見なされる
-	// var arr5 [4]string
-	// arr5 = arr1
-	// fmt.Println(arr5)
-
-	// 要素数を調べる
-	fmt.Println(len(arr1))
+	// x = 2
+	// fmt.Println(x + 3)
 }
