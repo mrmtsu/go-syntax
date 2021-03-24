18f5f8ba49c7a30d72a44c5fd02ff9ceabfa2548
mrmtsu <81202muramatsu@gmail.com>
2021-03-25 05:36:04 +0900

    配列型

--- main.go
+++ main.go
@@ -3,13 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	byteA := []byte{72, 73}
-	fmt.Println(byteA)
+	var arr1 [3]int
+	fmt.Println(arr1)
+	fmt.Printf("%T\n", arr1)
 
-	fmt.Println(string(byteA))
+	var arr2 [3]string = [3]string{"A", "B"}
+	fmt.Println(arr2)
 
-	c := []byte("HI")
-	fmt.Println(c)
+	arr3 := [3]int{1, 2, 3}
+	fmt.Println(arr3)
 
-	fmt.Println(string(c))
+	// 要素の数を自動でカウントする
+	arr4 := [...]string{"C", "D"}
+	fmt.Println(arr4)
+	fmt.Printf("%T\n", arr4)
+
+	fmt.Println(arr2[0])
+	fmt.Println(arr2[1])
+	fmt.Println(arr2[2])
+	fmt.Println(arr2[2-1])
+
+	arr2[2] = "C"
+	fmt.Println(arr2)
+
+	// 要素数が異なると別の型と見なされる
+	// var arr5 [4]string
+	// arr5 = arr1
+	// fmt.Println(arr5)
+
+	// 要素数を調べる
+	fmt.Println(len(arr1))
 }
