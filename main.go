a097a4da9bd0f63ff3fddb0027a59151dc0f5c8a
mrmtsu <81202muramatsu@gmail.com>
2021-03-25 05:15:02 +0900

    int型

--- main.go
+++ main.go
@@ -2,51 +2,19 @@ package main
 
 import "fmt"
 
-func outer() {
-	var s4 string = "outer"
-	fmt.Println(s4)
-}
-
 func main() {
-	// 明示的な定義
-	// var 変数名 型 = 値
 	var i int = 100
-	fmt.Println(i)
 
-	var s string = "Hello Go"
-	fmt.Println(s)
+	var i2 int64 = 200
 
-	var t, f bool = true, false
-	fmt.Println(t, f)
+	fmt.Println(i + 50)
 
-	var (
-		i2 int    = 200
-		s2 string = "Golang"
-	)
-	fmt.Println(i2, s2)
+	// 別の型となるためエラーとなる
+	// fmt.Println(i + i2)
 
-	var i3 int
-	var s3 string
-	fmt.Println(i3, s3)
+	fmt.Printf("%T\n", i2)
 
-	i3 = 300
-	s3 = "Go"
-	fmt.Println(i3, s3)
+	fmt.Printf("%T\n", int32(i2))
 
-	i = 150
-	fmt.Println(i)
-
-	// 暗黙的な定義
-	// 変数名 := 値
-	i4 := 400
-	fmt.Println(i4)
-
-	i4 = 450
-	fmt.Println(i4)
-
-	// i4 = "Hello"
-	// fmt.Println(i4)
-
-	outer()
-
+	fmt.Println(i + int(i2))
 }
