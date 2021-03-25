1e6145cab3ddcb4ac23bf3d45c36eeadf7f49aac
mrmtsu <81202muramatsu@gmail.com>
2021-03-26 06:02:26 +0900

    go routine

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func sub() {
+	for {
+		fmt.Println("Sub Loop")
+		time.Sleep(100 * time.Millisecond)
+	}
+}
 
 func main() {
-	defer func() {
-		if x := recover(); x != nil {
-			fmt.Println(x)
-		}
-	}()
-	panic("runtime error")
-	fmt.Println("Start")
+	go sub()
+	go sub()
+
+	for {
+		fmt.Println("Main Loop")
+		time.Sleep(200 * time.Millisecond)
+	}
 }
