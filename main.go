5eee74f68c594ad02e2c24f82c62069002b90a22
mrmtsu <81202muramatsu@gmail.com>
2021-03-26 06:53:48 +0900

    スライス

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import "fmt"
 
-func main() {
-	sl := []string{"A", "B", "C"}
-	fmt.Println(sl)
-
-	// for i, v := range sl {
-	// 	fmt.Println(i, v)
-	// }
-
-	for i := 0; i < len(sl); i++ {
-		fmt.Println(sl[i])
+func Sum(s ...int) int {
+	n := 0
+	for _, v := range s {
+		n += v
 	}
+	return n
+}
+
+func main() {
+	fmt.Println(Sum(1, 2, 3))
 }
