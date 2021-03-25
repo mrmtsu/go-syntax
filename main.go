a7e458955dc9043c2de906018048072fe7e835cd
mrmtsu <81202muramatsu@gmail.com>
2021-03-26 07:00:57 +0900

    map

--- main.go
+++ main.go
@@ -2,14 +2,32 @@ package main
 
 import "fmt"
 
-func Sum(s ...int) int {
-	n := 0
-	for _, v := range s {
-		n += v
-	}
-	return n
-}
-
 func main() {
-	fmt.Println(Sum(1, 2, 3))
+	var m = map[string]int{"A": 100, "B": 200}
+	fmt.Println(m)
+
+	m2 := map[string]int{"A": 100, "B": 200}
+	fmt.Println(m2)
+
+	m3 := map[int]string{
+		1: "A",
+		2: "B",
+	}
+	fmt.Println(m3)
+
+	m4 := make(map[int]string)
+	fmt.Println(m4)
+
+	m4[1] = "JAPAN"
+	m4[2] = "USA"
+	fmt.Println(m4)
+
+	fmt.Println(m["A"])
+	fmt.Println(m4[2])
+
+	s, ok := m4[3]
+	if !ok {
+		fmt.Println("error")
+	}
+	fmt.Println(s, ok)
 }
