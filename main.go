07d755fb156fe0a69d3a39407bab3a029eef0a66
mrmtsu <81202muramatsu@gmail.com>
2021-03-26 06:20:13 +0900

    copy

--- main.go
+++ main.go
@@ -3,29 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	sl := []int{100, 200}
-	fmt.Println(sl)
+	// sl := []int{100, 200}
+	// sl2 := sl
 
-	sl = append(sl, 300)
-	fmt.Println(sl)
+	// sl2[0] = 1000
+	// fmt.Println(sl)
 
-	sl = append(sl, 400, 500, 600)
-	fmt.Println(sl)
+	// var i int = 10
+	// i2 := i
+	// i2 = 100
+	// fmt.Println(i, i2)
 
-	sl2 := make([]int, 5)
-	fmt.Println(sl2)
+	sl := []int{1, 2, 3, 4, 5}
+	sl2 := make([]int, 5, 10)
+	n := copy(sl2, sl)
 
-	fmt.Println(len(sl2))
-
-	fmt.Println(cap(sl2))
-
-	sl3 := make([]int, 5, 10)
-	fmt.Println(len(sl3))
-
-	fmt.Println(cap(sl3))
-
-	sl3 = append(sl3, 1, 2, 3, 4, 5, 6, 7)
-	fmt.Println(len(sl3))
-
-	fmt.Println(cap(sl3))
+	fmt.Println(n, sl2)
 }
