b2d38948ce554e6ec5fe1bcd530ed99c8ffe2a8e
mrmtsu <81202muramatsu@gmail.com>
2021-03-26 05:59:10 +0900

    panic

--- main.go
+++ main.go
@@ -1,37 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-func TestDefer() {
-	defer fmt.Println("END")
-	fmt.Println("START")
-}
-
-func RunDefer() {
-	defer fmt.Println("1")
-	defer fmt.Println("2")
-	defer fmt.Println("3")
-}
+import "fmt"
 
 func main() {
-	TestDefer()
-
-	// defer func() {
-	// 	fmt.Println("1")
-	// 	fmt.Println("2")
-	// 	fmt.Println("3")
-	// }()
-
-	RunDefer()
-
-	file, err := os.Create("test.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-
-	file.Write([]byte("Hello"))
+	defer func() {
+		if x := recover(); x != nil {
+			fmt.Println(x)
+		}
+	}()
+	panic("runtime error")
+	fmt.Println("Start")
 }
