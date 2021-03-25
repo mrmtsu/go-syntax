ff185a28f8b2e0914603a9065c7965eb10bc71bf
mrmtsu <81202muramatsu@gmail.com>
2021-03-26 07:21:59 +0900

    chanel

--- main.go
+++ main.go
@@ -3,12 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string]int{
-		"Apple":  100,
-		"Banana": 200,
-	}
+	var ch1 chan int
 
-	for k, v := range m {
-		fmt.Println(k, v)
-	}
+	// 受信専用
+	// var ch2 <-chan int
+
+	// 送信専用
+	// var ch3 chan<- int
+
+	ch1 = make(chan int)
+
+	ch2 := make(chan int)
+
+	fmt.Println(cap(ch1))
+	fmt.Println(cap(ch2))
+
+	ch3 := make(chan int, 5)
+	fmt.Println(cap(ch3))
+
+	ch3 <- 1
+	fmt.Println(len(ch3))
+
+	ch3 <- 2
+	ch3 <- 3
+	fmt.Println(len(ch3))
+
+	i := <-ch3
+	fmt.Println(i)
+	fmt.Println("len", len(ch3))
+
+	i2 := <-ch3
+	fmt.Println(i2)
+	fmt.Println("len", len(ch3))
+
+	fmt.Println(<-ch3)
+	fmt.Println("len", len(ch3))
 }
